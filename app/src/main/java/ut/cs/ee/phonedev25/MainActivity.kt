26106547a8117a51b46dedad3e1b5abc769ea7ba
Mirko Martjak <mirkomar@ut.ee>
2025-10-01 13:13:31 +0300

    Created Android project inside of RESPO
    Created Android project inside of RESPO

---
+++ app/src/main/java/ut/cs/ee/phonedev25/MainActivity.kt
@@ -0,0 +1,47 @@
+package ut.cs.ee.phonedev25
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ut.cs.ee.phonedev25.ui.theme.Phonedev25Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Phonedev25Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Phonedev25Theme {
+        Greeting("Android")
+    }
+}
