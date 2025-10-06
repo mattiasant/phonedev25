245f795e27e82be769dc6d7e730757bb504701cd
Mirko Martjak <mirkomar@ut.ee>
2025-10-06 16:03:51 +0300

    Added new pages, design
    Added Statistics, Store page, Settings page and finished Join Game page. Changed design a little bit.

---
+++ app/src/main/java/ut/cs/ee/phonedev25/StorePage.kt
@@ -0,0 +1,20 @@
+package ut.cs.ee.phonedev25
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class StorePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_store_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
